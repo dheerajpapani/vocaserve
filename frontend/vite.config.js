637b1160798b1c45a4558a9e7185daefe660d18d
dheerajpapani <dheerajpapani@gmail.com>
2025-06-14 16:36:06 +0530

    Fix GitHub Pages routing with 404.html

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/vocaserve/',
   plugins: [react()],
-    server: {
-    proxy: {
-      '/vocabularies': 'https://vocaserve-backend.onrender.com'
-    }
-  }
 })
