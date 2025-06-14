ac6be696656df4b3c42dfc58bac6d64e5223d844
dheerajpapani <dheerajpapani@gmail.com>
2025-06-14 15:17:25 +0530

    feat: update routing to use HashRouter and add predeploy script

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import { AuthProvider } from './context/AuthContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </BrowserRouter>
+  <React.StrictMode>
+    <HashRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </HashRouter>
+  </React.StrictMode>
 );
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/vocaserve/',
   plugins: [react()],
+    server: {
+    proxy: {
+      '/vocabularies': 'https://vocaserve-backend.onrender.com'
+    }
+  }
 })
